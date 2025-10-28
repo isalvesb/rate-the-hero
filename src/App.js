4ae0a8073aa38c220da0188cc0b408d445bc0e75
isalvesb <alvesisamara254@gmail.com>
2025-10-28 15:09:07 -0300

    feat: create HeroCard component and Search screen structure

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Search } from "./screens/Search";
 import { Details } from "./screens/Details";
 import { NotFound } from "./screens/NotFound";
+import { NormalizeStyles } from "./shared/NormalizeStyles";
+import { Header } from "./common-components/Header/Header"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Search />} />
-        <Route path="/detalhes/:id" element={<Details />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <NormalizeStyles />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/detalhes/:id" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
